edea49142e5c605ffab8ae31407d34ea4afcf06b
SweetCuteWitch <81059271+SweetCuteWitch@users.noreply.github.com>
2021-03-21 05:13:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -94,100 +94,100 @@ letters.onclick = function switchLetters(){
 function playSound(elem){
     switch(elem.value){
         case "c♯":
-            var audio = new Audio('assets/audio/c♯.mp3');
+            var audio = new Audio('assets/c♯.mp3');
             audio.play();
             break;
         case "d♯":
-            var audio = new Audio('assets/audio/d♯.mp3');
+            var audio = new Audio('assets/d♯.mp3');
             audio.play();
             break;
         case "f♯":
-            var audio = new Audio('assets/audio/f♯.mp3');
+            var audio = new Audio('assets/f♯.mp3');
             audio.play();
             break;
         case "g♯":
-            var audio = new Audio('assets/audio/g♯.mp3');
+            var audio = new Audio('assets/g♯.mp3');
             audio.play();
             break;
         case "a♯":
-            var audio = new Audio('assets/audio/a♯.mp3');
+            var audio = new Audio('assets/a♯.mp3');
             audio.play();
             break;
         case "c":
-            var audio = new Audio('assets/audio/c.mp3');
+            var audio = new Audio('assets/c.mp3');
             audio.play();
             break;
         case "d":
-            var audio = new Audio('assets/audio/d.mp3');
+            var audio = new Audio('assets/d.mp3');
             audio.play();
             break;
         case "e":
-            var audio = new Audio('assets/audio/e.mp3');
+            var audio = new Audio('assets/e.mp3');
             audio.play();
             break;
         case "f":
-            var audio = new Audio('assets/audio/f.mp3');
+            var audio = new Audio('assets/f.mp3');
             audio.play();
             break;
         case "g":
-            var audio = new Audio('assets/audio/g.mp3');
+            var audio = new Audio('assets/g.mp3');
             audio.play();
             break;
         case "a":
-            var audio = new Audio('assets/audio/a.mp3');
+            var audio = new Audio('assets/a.mp3');
             audio.play();
             break;
         case "b":
-            var audio = new Audio('assets/audio/b.mp3');
+            var audio = new Audio('assets/b.mp3');
             audio.play();
             break; 
 
         case "R":
-            var audio = new Audio('assets/audio/c♯.mp3');
+            var audio = new Audio('assets/c♯.mp3');
             audio.play();
             break;
         case "T":
-            var audio = new Audio('assets/audio/d♯.mp3');
+            var audio = new Audio('assets/d♯.mp3');
             audio.play();
             break;
         case "U":
-            var audio = new Audio('assets/audio/f♯.mp3');
+            var audio = new Audio('assets/f♯.mp3');
             audio.play();
             break;
         case "I":
-            var audio = new Audio('assets/audio/g♯.mp3');
+            var audio = new Audio('assets/g♯.mp3');
             audio.play();
             break;
         case "O":
-            var audio = new Audio('assets/audio/a♯.mp3');
+            var audio = new Audio('assets/a♯.mp3');
             audio.play();
             break;
         case "D":
-            var audio = new Audio('assets/audio/c.mp3');
+            var audio = new Audio('assets/c.mp3');
             audio.play();
             break;
         case "F":
-            var audio = new Audio('assets/audio/d.mp3');
+            var audio = new Audio('assets/d.mp3');
             audio.play();
             break;
         case "G":
-            var audio = new Audio('assets/audio/e.mp3');
+            var audio = new Audio('assets/e.mp3');
             audio.play();
             break;
         case "H":
-            var audio = new Audio('assets/audio/f.mp3');
+            var audio = new Audio('assets/f.mp3');
             audio.play();
             break;
         case "J":
-            var audio = new Audio('assets/audio/g.mp3');
+            var audio = new Audio('assets/g.mp3');
             audio.play();
             break;
         case "K":
-            var audio = new Audio('assets/audio/a.mp3');
+            var audio = new Audio('assets/a.mp3');
             audio.play();
             break;
         case "L":
-            var audio = new Audio('assets/audio/b.mp3');
+            var audio = new Audio('assets/b.mp3');
             audio.play();
             break; 
     }
@@ -195,7 +195,7 @@ function playSound(elem){
 
 piano.addEventListener('keydown',(event) =>{
     if(event.key==="d" || event.key==="D" || event.key==="в" || event.key==="В"){
-        var audio = new Audio('assets/audio/c.mp3');
+        var audio = new Audio('assets/c.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "c" || elem.value == "D"){
@@ -204,7 +204,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="f" || event.key==="F" || event.key==="А" || event.key==="а"){
-        var audio = new Audio('assets/audio/d.mp3');
+        var audio = new Audio('assets/d.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "d" || elem.value == "F"){
@@ -213,7 +213,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="g" || event.key==="G" || event.key==="П" || event.key==="п"){
-        var audio = new Audio('assets/audio/e.mp3');
+        var audio = new Audio('assets/e.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "e" || elem.value == "G"){
@@ -222,7 +222,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="h" || event.key==="H" || event.key==="Р" || event.key==="р"){
-        var audio = new Audio('assets/audio/f.mp3');
+        var audio = new Audio('assets/f.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "f" || elem.value == "H"){
@@ -231,7 +231,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="j" || event.key==="J" || event.key==="О" || event.key==="о"){
-        var audio = new Audio('assets/audio/g.mp3');
+        var audio = new Audio('assets/g.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "g" || elem.value == "J"){
@@ -240,7 +240,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="k" || event.key==="K" || event.key==="л" || event.key==="Л"){
-        var audio = new Audio('assets/audio/a.mp3');
+        var audio = new Audio('assets/a.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "a" || elem.value == "K"){
@@ -249,7 +249,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="l" || event.key==="L" || event.key==="д" || event.key==="Д"){
-        var audio = new Audio('assets/audio/b.mp3');
+        var audio = new Audio('assets/b.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "b" || elem.value == "L"){
@@ -258,7 +258,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="r" || event.key==="R" || event.key==="к" || event.key==="К"){
-        var audio = new Audio('assets/audio/c♯.mp3');
+        var audio = new Audio('assets/c♯.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "c♯" || elem.value == "R"){
@@ -267,7 +267,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="t" || event.key==="T" || event.key==="Е" || event.key==="е"){
-        var audio = new Audio('assets/audio/d♯.mp3');
+        var audio = new Audio('assets/d♯.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "d♯" || elem.value == "T"){
@@ -276,7 +276,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="u" || event.key==="U" || event.key==="г" || event.key==="Г"){
-        var audio = new Audio('assets/audio/f♯.mp3');
+        var audio = new Audio('assets/f♯.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "f♯" || elem.value == "U"){
@@ -285,7 +285,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="I" || event.key==="i" || event.key==="ш" || event.key==="Ш"){
-        var audio = new Audio('assets/audio/g♯.mp3');
+        var audio = new Audio('assets/g♯.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "g♯" || elem.value == "I"){
@@ -294,7 +294,7 @@ piano.addEventListener('keydown',(event) =>{
         })
     }
     if(event.key==="o" || event.key==="O" || event.key==="щ" || event.key==="Щ"){
-        var audio = new Audio('assets/audio/a♯.mp3');
+        var audio = new Audio('assets/a♯.mp3');
         audio.play();
         COLLECTION.forEach((elem) =>{
             if(elem.value == "a♯" || elem.value == "O"){
